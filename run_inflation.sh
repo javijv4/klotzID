c4230ace9b36dd6f80a202a10401af079f7125bf
Javiera Jilberto <jilberto@umich.edu>
2024-02-27 10:45:35 -0500

    fixing small error in bash file

--- run_inflation.sh
+++ run_inflation.sh
@@ -3,6 +3,6 @@ k=$1
 kb=$2
 outdir=$3
 ncores=$4
-ncores=$5
+pfile=$5
 
-mpirun -np ${ncores} cheartsolver.out ${5} -\#k=${k} -\#kb=${kb} -\#outdir=${outdir}
+mpirun -np ${ncores} cheartsolver.out ${pfile} -\#k=${k} -\#kb=${kb} -\#outdir=${outdir}
